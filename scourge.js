04532b6a21bcd10751198d7dc36b74209f4e013f
SementeDeCacau <mesquitegamer73@gmail.com>
2017-11-16 20:24:38 -0200

    Update scourge.js

--- scourge.js
+++ scourge.js
@@ -5,13 +5,13 @@ bot.login('MzgwNzQ2OTIxMzU2Mjk2MTky.DO9FwQ.eUFX63NREeU7YtT1G_8WkqdMwXk');
 
 bot.on('guildMemberAdd', member => {
 
-    console.log('User' + mentions.user.username + 'joined the server!')
+    console.log('User' + member.user.username + 'joined the server!')
     console.log(member)
 
-    member.guild.channels.get('380843437152534551').send('Welcome, ' + mentions.user.username + ', please read the rules')
+    member.guild.channels.get('380843437152534551').send('Welcome, ' + member.user.username + ', please read the rules')
 });
 
 bot.on('guildMemberRemove', member => {
-    member.guild.channels.get('380843437152534551').send('' + mentions.user.username + 'left...')
+    member.guild.channels.get('380843437152534551').send('' + member.user.username + 'left...')
 
 });
