f816249c11d5a6ec166c6145d9c598ea5cdbe773
SementeDeCacau <mesquitegamer73@gmail.com>
2017-11-16 14:56:48 -0200

    Create scourge.js

---
+++ scourge.js
@@ -0,0 +1,19 @@
+const botSettings = require("./botsettings.json");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = botSettings.prefix;
+
+bot.login('MzgwNzQ2OTIxMzU2Mjk2MTky.DO9FwQ.eUFX63NREeU7YtT1G_8WkqdMwXk');
+
+bot.on('guildMemberAdd', member => {
+
+    console.log('User' + member.user.username + 'joined the server!')
+    console.log(member)
+
+    member.guild.channels.get('373244635293941764').send('**' + member.user.username + '** has joined us!')
+});
+
+bot.on('guildMemberRemove', member => {
+    member.guild.channels.get('373244635293941764').send('**' + member.user.username + '** left...')
+
+});
